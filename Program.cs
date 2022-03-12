8ec43647c750f284ad693b8b8845619cac9da585
Alexander Karpukhin <thealexdev23@gmail.com>
2022-03-12 19:24:09 +0100

    some more changes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@
     return;
 }
 
+// I hate to do this, but didn't find any other way :(
+var hoursPreviousEqualToZero   = false;
+var minutesPreviousEqualToZero = false;
+
 var time = args[0];
 
 var minutes = 0;
@@ -12,19 +16,74 @@ var hours   = 0;
 AnalyzeTime();
 FormatTime();
 
-Console.Clear();
-Console.CursorVisible = false;
 
-Console.WriteLine(hours + " " + minutes +  " " + seconds);
+ConfigureConsoleToStart();
 
 //TODO: every second change the text on the screen and subtract 1 second from the total timer
-//TODO: create something capable of drawing time in the following format hh:mm:ss 
-while(true) {
-    
+//TODO: create something capable of drawing time in the following format hh:mm:ss
+while (true) {
+    seconds--;
+    FormatTime();
+
+    string timeToPrintString = ""; 
+
+    if (minutes > 0)
+        timeToPrintString += seconds.ToString("00");
+    else
+        timeToPrintString += seconds.ToString();
+
+    timeToPrintString += ":";
+
+    if (hours > 0) 
+        timeToPrintString += minutes.ToString("00");
+    else 
+        timeToPrintString += minutes.ToString();
+
+    timeToPrintString += ":";
+
+    timeToPrintString += hours.ToString();
+
+    Console.WriteLine(timeToPrintString);
     break;
+
+    int count = 0;
+
+    foreach(var t in timeToPrintString) {
+        Console.CursorLeft = 8 * count;
+        Console.CursorTop  = 0;
+
+        string printResult = "";
+
+        if (t != ':') {
+            printResult = GetAsciiByNum(Convert.ToInt32(t));
+        } else {
+            printResult = GetAsciiByNum(10);
+        }
+
+        foreach(var s in printResult) {
+            if (s != '\n') {
+                Console.Write(s);
+            } else {
+                Console.CursorTop++;
+            }
+        }
+        count++;
+    }
+
+    if((hours == minutes) && (minutes == seconds) && (seconds == 0)) {
+       break; 
+    }
+
+    Thread.Sleep(1);
 }
 
-Console.CursorVisible = true;
+ConfigureConsoleToEnd();
+
+/*
+
+All the functions
+
+*/
 
 // Would set values of Seconds Minutes and Hours according to the Time value
 void AnalyzeTime() {
@@ -32,9 +91,9 @@ void AnalyzeTime() {
     string? minutesStr = null;
     string? hoursStr   = null;
     var buffer         = "";
-    
-    foreach (var t in time)
-    {
+
+    //Itereate for every letter in the time argument and set the time minutes and seconds variables
+    foreach (var t in time) {
         switch(t) {
             case 's':
                 secondsStr = buffer;
@@ -54,15 +113,12 @@ void AnalyzeTime() {
         }
     }
 
-    if (secondsStr != null) {
-        seconds = Convert.ToInt32(secondsStr);
-    }
-    if (minutesStr != null) {
-        minutes = Convert.ToInt32(minutesStr);
-    }
-    if (hoursStr != null) {
-       hours = Convert.ToInt32(hoursStr);
-    }
+    if (secondsStr is not null) seconds = Convert.ToInt32(secondsStr);
+    if (minutesStr is not null) minutes = Convert.ToInt32(minutesStr);
+    if (hoursStr is not null)   hours   = Convert.ToInt32(hoursStr);
+    
+    if (hours == 0)   hoursPreviousEqualToZero   = true;
+    if (minutes == 0) minutesPreviousEqualToZero = true;
 }
 
 /*
@@ -71,6 +127,7 @@ void AnalyzeTime() {
  * This function would convert it to:
  * 3h2m
  */
+
 void FormatTime() {
     while(seconds >= 60) {
         minutes++;
@@ -81,6 +138,36 @@ void FormatTime() {
         hours++;
         minutes -= 60;
     }
+
+    if(hours == 0 && !hoursPreviousEqualToZero) {
+        minutes = 59;
+        seconds = 60;
+        hoursPreviousEqualToZero = true;
+    } else {
+        hoursPreviousEqualToZero = false;
+    }
+    
+    if(minutes == 0 && !minutesPreviousEqualToZero) {
+        hours--;
+        minutes = 59;
+        minutesPreviousEqualToZero = true;
+    } else {
+        minutesPreviousEqualToZero = false;
+    }
+
+    if(seconds == 0) {
+        minutes--;
+        seconds = 60;
+    }
+}
+
+void ConfigureConsoleToStart() {
+    Console.Clear();
+    Console.CursorVisible = false;
+}
+
+void ConfigureConsoleToEnd() {
+    Console.CursorVisible = true;
 }
 
 // Some hardcoded values
@@ -88,104 +175,104 @@ string GetAsciiByNum(int num) {
     string result;
     switch(num) {
         case 0:
-            result = "  ####  \n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= "  ####  \n";
+            result = "  ████  \n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= "  ████  \n";
             break;
         case 1:
-            result = "  ###   \n";
-            result+= "## ##   \n";
-            result+= "   ##   \n";
-            result+= "   ##   \n";
-            result+= "   ##   \n";
-            result+= "   ##   \n";
-            result+= "########\n";
+            result = "  ███   \n";
+            result+= "██ ██   \n";
+            result+= "   ██   \n";
+            result+= "   ██   \n";
+            result+= "   ██   \n";
+            result+= "   ██   \n";
+            result+= "████████\n";
             break;
         case 2:
-            result = " #######\n";
-            result+= "##    ##\n";
-            result+= "     ## \n";
-            result+= "   ##   \n";
-            result+= " ##     \n";
-            result+= " ##     \n";
-            result+= " #######\n";
+            result = " ███████\n";
+            result+= "██    ██\n";
+            result+= "     ██ \n";
+            result+= "   ██   \n";
+            result+= " ██     \n";
+            result+= " ██     \n";
+            result+= " ███████\n";
             break;
         case 3:
-            result = " #######\n";
-            result+= "##    ##\n";
-            result+= "      ##\n";
-            result+= "   ###  \n";
-            result+= "      ##\n";
-            result+= "##    ##\n";
-            result+= " #######\n";
+            result = " ███████\n";
+            result+= "██    ██\n";
+            result+= "      ██\n";
+            result+= "   ███  \n";
+            result+= "      ██\n";
+            result+= "██    ██\n";
+            result+= " ███████\n";
             break;
        
         case 4:
-            result = "##    ##\n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= "######## \n";
-            result+= "      ##\n";
-            result+= "      ##\n";
-            result+= "      ##\n";
+            result = "██    ██\n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= "████████ \n";
+            result+= "      ██\n";
+            result+= "      ██\n";
+            result+= "      ██\n";
             break;
         case 5:
-            result = "########\n";
-            result+= "##      \n";
-            result+= "##      \n";
-            result+= " ###### \n";
-            result+= "      ##\n";
-            result+= "      ##\n";
-            result+= "########\n";
+            result = "████████\n";
+            result+= "██      \n";
+            result+= "██      \n";
+            result+= " ██████ \n";
+            result+= "      ██\n";
+            result+= "      ██\n";
+            result+= "████████\n";
             break;
         case 6:
-            result = " #######\n";
-            result+= "##      \n";
-            result+= "##      \n";
-            result+= " ###### \n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= " #######\n";
+            result = " ███████\n";
+            result+= "██      \n";
+            result+= "██      \n";
+            result+= " ██████ \n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= " ███████\n";
             break;
         case 7:
-            result = "########\n";
-            result+= "      ##\n";
-            result+= "      ##\n";
-            result+= "     ## \n";
-            result+= "    ##  \n";
-            result+= "    ##  \n";
-            result+= "    ##  \n";
+            result = "████████\n";
+            result+= "      ██\n";
+            result+= "      ██\n";
+            result+= "     ██ \n";
+            result+= "    ██  \n";
+            result+= "    ██  \n";
+            result+= "    ██  \n";
             break;
         case 8:
-            result = " ###### \n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= " ###### \n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= " ###### \n";
+            result = " ██████ \n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= " ██████ \n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= " ██████ \n";
             break;
         case 9:
-            result = " ###### \n";
-            result+= "##    ##\n";
-            result+= "##    ##\n";
-            result+= " ###### \n";
-            result+= "      ##\n";
-            result+= "      ##\n";
-            result+= " ###### \n";
+            result = " ██████ \n";
+            result+= "██    ██\n";
+            result+= "██    ██\n";
+            result+= " ██████ \n";
+            result+= "      ██\n";
+            result+= "      ██\n";
+            result+= " ██████ \n";
             break;
         case 10:
-            result = "   ##   \n";
-            result+= "   ##   \n";
+            result = "   ██   \n";
+            result+= "   ██   \n";
             result+= "        \n";
             result+= "        \n";
             result+= "        \n";
-            result+= "   ##   \n";
-            result+= "   ##   \n";
+            result+= "   ██   \n";
+            result+= "   ██   \n";
             break;
         default:
             result = "No Num Found";
